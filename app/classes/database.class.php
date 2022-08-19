1109a60114773f5ae02167bc2bde4a6d5832928a
Ricardo Bavaresco <ricardo.bav17@gmail.com>
2022-08-19 11:28:56 -0300

    configurada senha do banco teste

--- app/classes/database.class.php
+++ app/classes/database.class.php
@@ -4,7 +4,7 @@ class Database {
 
     private $host   = "localhost";
     private $user   = "root";
-    private $pwd    = "";
+    private $pwd    = "admin";
     private $dbName = "teste";
 
     protected function connect() {
